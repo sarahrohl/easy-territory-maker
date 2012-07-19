ffa9ce083c7793610c3fcf28fd0ba2b57e48dad8
vallevistaterritory <vallevistaterritory@ffa539ec-108e-55aa-9b01-7748e89027da>
2012-07-19 12:43:56 +0000

    [ENH] jQuery goes to google now

--- trunk/index.php
+++ trunk/index.php
@@ -1,11 +1,22 @@
+<!DOCTYPE html>
 <html>
 <head>
 	<title>Easy Territory Maker</title>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+	<script src="http://code.jquery.com/ui/1.8.21/jquery-ui.min.js"></script>
+	<script src="http://jquery-ui.googlecode.com/svn/tags/latest/ui/minified/i18n/jquery-ui-i18n.min.js"></script>
+	<link href="http://jquery-ui.googlecode.com/svn/tags/latest/themes/smoothness/jquery.ui.all.css" type="text/css" rel="Stylesheet" />
+	<script>
+		$(function() {
+			$('#maps').accordion({
+				autoHeight: false
+			});
+		});
+	</script>
 </head>
+<div id="maps">
 <?php
-
 include_once('lib/etm.php');
-
 $etm = new etm();
 
 foreach($etm->all() as $locality) {
@@ -19,4 +30,5 @@ foreach($etm->all() as $locality) {
 	echo "</ul>";
 }
 ?>
+</div>
 </html>
