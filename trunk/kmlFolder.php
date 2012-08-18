127d3c0c869df43da67a41105a2e51b60722fdab
vallevistaterritory <vallevistaterritory@ffa539ec-108e-55aa-9b01-7748e89027da>
2012-08-18 19:21:39 +0000

    Minor updates for viewing

--- trunk/kmlFolder.php
+++ trunk/kmlFolder.php
@@ -5,6 +5,25 @@ include_once('lib/etm.php');
 $etm = new etm();
 
 header ("Content-Type:text/xml"); 
+
+$borderColor = '';
+$fillColor = '';
+
+switch (!empty($_REQUEST['locality']) ? $_REQUEST['locality'] : 'Apartment') {
+	case "Business":
+		$borderColor = '660000ff';
+		$fillColor = '660000ff';
+		break;
+	default:
+		$borderColor = '4000ff00';
+		$fillColor = '00000000';
+		break;
+}
+
+if (isset($_REQUEST['mini'])) {
+	$borderColor = '660000ff';
+}
+
 echo '
 <kml xmlns="http://www.opengis.net/kml/2.2" xmlns:gx="http://www.google.com/kml/ext/2.2" xmlns:kml="http://www.opengis.net/kml/2.2" xmlns:atom="http://www.w3.org/2005/Atom">
 	<Document>
@@ -12,15 +31,12 @@ echo '
 		<open>1</open>
 		<Style id="standardStyle">
 		<LineStyle>
-			<color>'.(isset($_REQUEST['mini']) ? 
-					'990000ff' : 
-					($_REQUEST['locality'] == 'Apartment' ? 
-						'990000ff' : 
-						'4000ff00'
-				)
-			).'</color>
+			<color>'.$borderColor.'</color>
 			<width>5</width>
 		</LineStyle>
+		<PolyStyle>
+			<color>' . $fillColor . '</color>
+		</PolyStyle>
 		</Style>
 		'.$etm->getMap($_REQUEST['locality'], $_REQUEST['map']).'
 	</Document>
