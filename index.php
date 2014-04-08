f48c9fd65cd5500c5f9ee83c0e53e791a8ac0d5c
vallevistaterritory <vallevistaterritory@gmail.com>
2014-04-08 20:01:50 +0000

    updated to use bower

--- index.php
+++ index.php
@@ -2,10 +2,11 @@
 <html>
 <head>
 	<title>Easy Territory Maker</title>
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-	<script src="http://code.jquery.com/ui/1.8.21/jquery-ui.min.js"></script>
-	<script src="http://jquery-ui.googlecode.com/svn/tags/latest/ui/minified/i18n/jquery-ui-i18n.min.js"></script>
-	<link href="http://jquery-ui.googlecode.com/svn/tags/latest/themes/smoothness/jquery.ui.all.css" type="text/css" rel="Stylesheet" />
+    <script src="bower_components/jquery/dist/jquery.js"></script>
+    <script src="bower_components/jquery-ui/ui/jquery-ui.js"></script>
+    <script src="bower_components/jquery-ui/ui/i18n/jquery-ui-i18n.js"></script>
+    <link href="bower_components/jquery-ui/themes/smoothness/jquery-ui.css" type="text/css" rel="Stylesheet" />
+
 	<script>
 		$(function() {
 			$('#maps').tabs();
