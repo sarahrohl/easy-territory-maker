bbd5e0e7cf846e1e71046d12e6f5e5febfa68857
vallevistaterritory@gmail.com <vallevistaterritory@gmail.com@ffa539ec-108e-55aa-9b01-7748e89027da>
2014-04-08 21:02:07 +0000

    Add in different method of including Placemarks.

--- trunk/index.php
+++ trunk/index.php
@@ -6,32 +6,28 @@
     <script src="bower_components/jquery-ui/ui/jquery-ui.js"></script>
     <script src="bower_components/jquery-ui/ui/i18n/jquery-ui-i18n.js"></script>
     <link href="bower_components/jquery-ui/themes/smoothness/jquery-ui.css" type="text/css" rel="Stylesheet" />
-
-	<script>
-		$(function() {
-			$('#maps').tabs();
-		});
-	</script>
 </head>
 <?php
 include_once('lib/etm.php');
 $etm = new etm();
 
-$tabs = "";
 $tabContents = "";
 foreach($etm->all() as $index => $locality) {
-	$tabs .= "<li><a href='#tab$index'>" . $locality->name . "</a></li>";
-
 	$tabContents .= "<div id='tab$index'><ul>";
-	foreach($locality as $map) {
-		if (!empty($map->name)) {
-			$tabContents .= "<li><a href='viewMap.php?map=" . $map->name . "&locality=" . $locality->name . "'>" . $map->name . "</a></li>";
-		}
-	}
+
+    if (!empty($locality->Placemark)) {
+        foreach($locality as $map) {
+            if (!empty($map->name)) {
+                $tabContents .= "<li><a href='viewMap.php?map=" . $map->name . "&locality=" . $locality->name . "'>" . $map->name . ' - ' . $locality->name . ' ' . "</a></li>";
+            }
+        }
+    } else {
+        $tabContents .= "<li><a href='viewMap.php?map=" . $locality->name . "'>" . $locality->name . "</a></li>";
+    }
 	$tabContents .= "</ul></div>";
 }
 ?>
 <div id="maps">
-	<?php echo "<ul>" . $tabs . "</ul>" . $tabContents?>
+	<?php echo $tabContents?>
 </div>
 </html>
