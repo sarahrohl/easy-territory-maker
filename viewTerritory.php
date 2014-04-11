ad54d0d1a5e9dc4ae6ca4463fecf5c77a514da39
vallevistaterritory <vallevistaterritory@gmail.com>
2014-04-11 15:56:21 +0000

    Move map layers around.

--- viewTerritory.php
+++ viewTerritory.php
@@ -10,7 +10,7 @@
     }
 ?><!DOCTYPE html>
 <html>
-  <head>
+<head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
     <meta name="apple-mobile-web-app-capable" content="yes">
@@ -21,12 +21,12 @@
     <script src="lib/Map.js"></script>
     <script src="bower_components/jquery-ui/ui/i18n/jquery-ui-i18n.js"></script>
     <link href="bower_components/jquery-ui/themes/smoothness/jquery-ui.css" type="text/css" rel="Stylesheet" />
-	
+
     <style type="text/css">
-		html, body {
+        html, body {
             height: 100%;
-			padding: 0;
-			margin: 0;
+            padding: 0;
+            margin: 0;
         }
         .olPopup p {
             margin:0px;
@@ -49,170 +49,174 @@
             padding: 0px;
             width: 100%;
         }
-
+    </style>
+<?php if (!isset($_REQUEST['debug'])) {?>
+    <style>
         .olControlAttribution,
         .olControlZoom,
-        .maximizeDiv {
+        .maximizeDiv,
+        .gmnoprint {
             display: none ! important;
         }
         .ui-resizable-handle {
             background-image: none ! important;
         }
     </style>
-    <script src="bower_components/OpenLayers/OpenLayers.js"></script>
+<?php }?>
+    <script src="bower_components/OpenLayers/lib/OpenLayers.js"></script>
     <script>
-		$(function() {
-			var height = $(window).height(),
-			    width = height * 1.6,
+        $(function() {
+            var height = $(window).height(),
+                width = height * 1.6,
                 map = $('#map'),
                 mapMini = $('#mapMini'),
-                territoryName = $('#mapId').val(),
+                territoryName = $('#territory').val(),
                 locality = $('#locality').val(),
                 card = $('#card');
 
-			mapMini
-				.height(height * 0.5);
-							
-			map
-				.height(height)
-				.width(width)
-				.resizable({
-					aspectRatio: 1.6,
-					alsoResize: '#card'
-				});
-				
-			card
-				.height(height)
-				.width(width)
-				.resizable({
-					aspectRatio: 1.6,
-					alsoResize: '#map',
-					stop: function() {
-						resetLabels();
-					}
-				})
-				.trigger('resize');
-			
-			resetLabels();
+            mapMini
+                .height(height * 0.5);
 
-            new Map(map, territoryName, locality);
-            new Map(mapMini, territoryName, locality, true);
+            map
+                .height(height)
+                .width(width)
+                .resizable({
+                    aspectRatio: 1.6,
+                    alsoResize: '#card'
+                });
+
+            card
+                .height(height)
+                .width(width)
+                .resizable({
+                    aspectRatio: 1.6,
+                    alsoResize: '#map',
+                    stop: function() {
+                        resetLabels();
+                    }
+                })
+                .trigger('resize');
+
+            resetLabels();
+
+            console.log(new Map(map, territoryName, locality));
+            console.log(new Map(mapMini, territoryName, locality, true));
         });
-		
-		function resetLabels() {
+
+        function resetLabels() {
             var map = $('#map'),
                 card = $('#card'),
                 mapPosition = map.position(),
                 cardPosition = card.position(),
-			    height = map.height(),
-			    width = height * 1.6;
-			
-			$('#cardLabel').css({
+                height = map.height(),
+                width = height * 1.6;
+
+            $('#cardLabel').css({
                 width: width + 'px',
                 left: '1px',
-                top: (cardPosition.top + (width * 0.07)) + 'px',
+                top: (cardPosition.top + (width * 0.08)) + 'px',
                 fontSize: (width * 0.04) + 'px'
             });
-			
-			$('#north').css({
+
+            $('#north').css({
                 position: 'absolute',
                 top: (mapPosition.top + 120) + 'px',
                 left: (mapPosition.left + 10) + 'px'
             });
-		}
-		
+        }
+
         function onPopupClose(evt) {
             select.unselectAll();
         }
     </script>
-  </head>
-  <body>
-	<input type="hidden" id="mapId" value="<?php echo $_REQUEST['territory']; ?>" />
-	<input type="hidden" id="locality" value="<?php echo $_REQUEST['locality']; ?>" />
-	<input type="hidden" id="congregation" value="<?php echo $_REQUEST['congregation']; ?>" />
-	
-	<img id="card" src="my_files/card.png" />
-	<table id="cardLabel" style="position: absolute;" border="0">
-		<tr>
-			<td style="width: 1%;"></td>
-			<td style="width: 10%;"></td>
-			<td style="width: 35%;"><?php echo $_REQUEST['congregation'] . ' ' . $_REQUEST['locality']; ?></td>
-			<td style="width: 1%;"></td>
-			<td style="width: 10%;"></td>
-			<td style="width: 12%;"><?php echo $_REQUEST['territory']; ?></td>
-			<td style="width: 1%;"></td>
-		</tr>
-		<tr>
-			<td></td>
-			<td colspan="2" style="text-align: center;">
-				<br />
-				<div id="mapMini" style="border: none;"></div>
-			</td>
-			<td></td>
-			<td style="font-size: 14px;" colspan="2">
-				<h3>Directions</h3>
-				<ul>
-					<li>Work area highlighted in <span style="color: green;">green</span> on back.</li>
-					<li>Keep track of do not calls on front.</li>
-				</ul>
-				<h3>Do Not Calls</h3>
-				<table style="width: 100%;" border="1">
-					<tr>
-						<th>Name</th>
-						<th>Address</th>
-					</tr>
-					<tr>
-						<td>&nbsp;</td>
-						<td>&nbsp;</td>
-					</tr>
-					<tr>
-						<td>&nbsp;</td>
-						<td>&nbsp;</td>
-					</tr>
-					<tr>
-						<td>&nbsp;</td>
-						<td>&nbsp;</td>
-					</tr>
-					<tr>
-						<td>&nbsp;</td>
-						<td>&nbsp;</td>
-					</tr>
-					<tr>
-						<td>&nbsp;</td>
-						<td>&nbsp;</td>
-					</tr>
-					<tr>
-						<td>&nbsp;</td>
-						<td>&nbsp;</td>
-					</tr>
-					<tr>
-						<td>&nbsp;</td>
-						<td>&nbsp;</td>
-					</tr>
-					<tr>
-						<td>&nbsp;</td>
-						<td>&nbsp;</td>
-					</tr>
-					<tr>
-						<td>&nbsp;</td>
-						<td>&nbsp;</td>
-					</tr>
-					<tr>
-						<td>&nbsp;</td>
-						<td>&nbsp;</td>
-					</tr>
-				</table>
-			</td>
-		</tr>
-		<tr>
-			<td></td>
-			<td style="font-size: 12px; color: red;text-align: center;" colspan="2">
-				(aerial map, larger map on back)
-			</td>
-		</tr>
-	</table>
-	<br style="line-height: 4px;" />
-	<div id="map" class="smallmap" style="border: none;"></div>
-	<img id="north" src="img/n.png" />
-  </body>
+</head>
+<body>
+    <input type="hidden" id="territory" value="<?php echo $_REQUEST['territory']; ?>" />
+    <input type="hidden" id="locality" value="<?php echo $_REQUEST['locality']; ?>" />
+    <input type="hidden" id="congregation" value="<?php echo $_REQUEST['congregation']; ?>" />
+
+    <img id="card" src="my_files/card.png" />
+    <table id="cardLabel" style="position: absolute;" border="0">
+        <tr>
+            <td style="width: 3%;"></td>
+            <td style="width: 10%;"></td>
+            <td style="width: 35%; white-space:nowrap;"><?php echo $_REQUEST['congregation'] . ' ' . $_REQUEST['locality']; ?></td>
+            <td style="width: 1%;"></td>
+            <td style="width: 10%;"></td>
+            <td style="width: 12%;"><?php echo $_REQUEST['territory']; ?></td>
+            <td style="width: 1%;"></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td colspan="2" style="text-align: center;">
+                <br />
+                <div id="mapMini" style="border: none;"></div>
+            </td>
+            <td></td>
+            <td style="font-size: 14px;" colspan="2">
+                <h3>Directions</h3>
+                <ul>
+                    <li>Work <span style="font-weight: bold;">entire highlighted area </span> in <span style="color: green;">green</span> on back.</li>
+                    <li>Keep track of do not calls on front.</li>
+                </ul>
+                <h3>Do Not Calls</h3>
+                <table style="width: 100%;" border="1">
+                    <tr>
+                        <th>Name</th>
+                        <th>Address</th>
+                    </tr>
+                    <tr>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                    </tr>
+                    <tr>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                    </tr>
+                    <tr>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                    </tr>
+                    <tr>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                    </tr>
+                    <tr>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                    </tr>
+                    <tr>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                    </tr>
+                    <tr>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                    </tr>
+                    <tr>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                    </tr>
+                    <tr>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                    </tr>
+                    <tr>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+        <tr>
+            <td></td>
+            <td style="font-size: 12px; color: red;text-align: center;" colspan="2">
+                (aerial map, larger map on back)
+            </td>
+        </tr>
+    </table>
+    <br style="line-height: 4px;" />
+    <div id="map" class="smallmap" style="border: none;"></div>
+    <img id="north" src="img/n.png" />
+</body>
 </html>
