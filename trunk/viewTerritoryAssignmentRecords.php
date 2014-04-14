bfacecfa212310896143896795942d21c3211bc0
vallevistaterritory@gmail.com <vallevistaterritory@gmail.com@ffa539ec-108e-55aa-9b01-7748e89027da>
2014-04-14 14:08:12 +0000

    Add territory assignment records

---
+++ trunk/viewTerritoryAssignmentRecords.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <title>Territory Assignment Records</title>
+</head>
+<body>
+    <b>COMING SOON</b>
+    <img id="card" src="my_files/s13.png" />
+</body>
+</html>
